6c30b963eecb9859ef974284feeea53bd779d530
luizfilipeyep <83364189+luizfilipeyep@users.noreply.github.com>
2023-02-16 14:06:46 -0300

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 
 import React from "react";
 import "./style.css";
+import "./App.css"
 
 import Welcome from "./components/Welcome/Welcome"
 
 export default function App() {
   return (
-    <div>
+    <div className="App">
       <h1>Quiz de Programação</h1>
       <Welcome />
     </div>
