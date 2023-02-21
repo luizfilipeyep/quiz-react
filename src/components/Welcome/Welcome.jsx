e6b06811db91b239e6434bd70079be3fc25dc4be
luizfilipeyep <83364189+luizfilipeyep@users.noreply.github.com>
2023-02-21 12:18:34 -0300

    2 files modified

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -7,15 +7,13 @@ import "./Welcome.css"
 import Quiz from "../../img/quiz.svg"
 
 export default function Welcome() {
-  const quizState = useContext(QuizContext)
-
-  console.log(quizState)
+  const [quizState, dispatch] = useContext(QuizContext)
 
   return (
     <div id="welcome">
       <h2>Seja bem-vindo</h2>
       <p>Clique no botão abaixo para começar:</p>
-      <button>Iniciar</button>
+      <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>Iniciar</button>
       <img src={Quiz} alt="Início do Quiz" />
     </div>
   );
