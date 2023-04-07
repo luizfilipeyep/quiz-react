85b355beaecea31c5fa46f208d2869cb7810661d
luizfilipeyep <83364189+luizfilipeyep@users.noreply.github.com>
2023-04-07 14:33:43 -0300

    changing stages

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import React from "react";
 import "./style.css";
 import "./App.css"
 
+import { useContext } from "react"
+import { QuizContext } from "./context/quiz";
+
 import Welcome from "./components/Welcome/Welcome"
+import Question from "./components/Question/Question"
 
 export default function App() {
+  const [quizState, dispatch] = useContext(QuizContext)
+
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
-      <Welcome />
+
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
     </div>
   );
 }
--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useContext } from "react";
+import { QuizContext } from "../../context/quiz";
 
 export default function Question() {
+  const [quizState, dispatch] = useContext(QuizContext)
+
  return (
    <div>
      
